3609e284217d4507e7bc425eadd813ef52f60a03
nearbyfreshpizza <98099193+nearbyfreshpizza@users.noreply.github.com>
2022-01-22 19:29:22 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,89 @@
+// window.onclick = function(event) {
+//   if(event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
 window.addEventListener('DOMContentLoaded', function(event) {
-  // loaded(event);
-  // logs(event);
-  // names();
-  buttons();
+  localstore()
+  cart()
 });
 
-function buttons() {
-  let marker = document.querySelectorAll('h1 > b');
-  for(var value of marker.values()) {
-    console.log(value);
+let orderPizza = async function () {
+  let order = await $$('#order');
+}
+
+function cart(button, type) {
+  let itms = 0,
+  amt = 0;
+  if (button) {
+    itms ++;
+    amt += type['Price'];
+    names += type['Name'];
   }
 }
 
-function loaded(event) { console.log("Event: " + event); }
+
 
 let restaurant = new Object();
+restaurant['Options'] = ['Order', 'Pizza', 'Pasta', 'Starters', 'Drinks', 'Delivery'];
+restaurant['Options'].map(o=>{
+  restaurant[o] = new Object();
+  // console.log(restaurant[o]);
+});
 
-restaurant['Options'] = ['Pizza', 'Pasta', 'Starters', 'Drinks', 'Delivery'];
-restaurant['Options'].map(o=>restaurant[o] = new Object());
+restaurant.Order['Pizzas']; //  = orderPizza();
+restaurant.Order['Customer']; //  = profile();
+restaurant.Order['Total']; //  = totalPrice();
 
 restaurant.Pizza['Names'] = ['Margherita', 'Formaggio', 'Chicken', "Pineapple'o'clock", 'Meat Town'];
 restaurant.Pizza['Sizes'] = ['Personal', 'Small', 'Medium', 'Large', 'Extra Large']
-restaurant.Pizza['Radii'] = [8, 10, 12, 14, 16]
-restaurant.Pizza['Areas'] = restaurant['Pizza']['Radii'].map(r=>parseFloat((3.1415926*r**2).toPrecision(4)));
-restaurant.Pizza['Prices'] = [10, 14, 18, 25, 30]
-restaurant.Pizza['Taxes'] = taxes('Pizza');
+restaurant.Pizza['Prices'] = [10, 14, 18, 25, 30];
 
 restaurant.Pasta['Names'] = ['Lasagna', 'Ravioli', 'Spaghetti Classica'];
 restaurant.Pasta['Prices'] = [10]
-restaurant.Pasta['Taxes'] = taxes('Pasta');
 
 restaurant.Starters['Names'] = ["Today's Salad", 'Bruschetta', 'Garlic Bread'];
 restaurant.Starters['Prices'] = [6.5, 8, 10]
-restaurant.Starters['Taxes'] = taxes('Starters');
 
 restaurant.Drinks['Names'] = ['Coke', '7 Up', 'Beer', 'Wine', 'Orange Juice'];
 restaurant.Drinks['Sizes'] = ['12 Ounce', '2 Litre'];
 restaurant.Drinks['Prices'] = [10, 14]
-restaurant.Drinks['Taxes'] = taxes('Drinks');
 
 restaurant.Delivery['fee'] = 2.50;
 restaurant.Delivery['tip'];
 
 function logs(event) {
   console.log(`Restaurant: ${JSON.stringify(restaurant)}`);
-  console.log(restaurant.Pizza['Taxes'])
 }
 
-function totalPrice() { let order = new Object(); }
-function choose() { let prices; }
-function names() {
-  restaurant.Pizza['Names'].map(name=>[b] = document.createElement('b'));
-  console.log([b]);
+class Order {
+  constructor(name, items, amount) {
+    this.name = name;
+    this.items = items;
+    this.amt = amount;
+  }
+  set newAmount(amount) {
+    this.amt += amount;
+  }
 }
-function taxes(opt) {
-  restaurant[opt]['Taxes'] = restaurant[opt]['Prices'].map(p=>parseFloat((p*.0725).toPrecision(4)));
-  return restaurant[opt]['Taxes']
+
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+function calculatePrice(items) {
+  restaurant[items].Price.reduce(reducer)
+  
+  let order = new Order(customerNumber, items, amount);
+}
+
+function localstore() {
+  localStorage.setItem('Restaurant', JSON.stringify(restaurant))
+}
+
+let $$ = async function(param) {
+  return document.querySelector(param);
+}
+
+async function buttons() {
+  let marker = await document.querySelectorAll('h1 > b');
+  for await (let value of marker.values()) { console.log(value) }
 }
